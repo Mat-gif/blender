10e3ecd304171fdb76b120a067202feacf313928
Cazeres Mathieu <mathieucazeres@gmail.com>
2023-03-19 14:55:19 +0100

    modification de quelque petites erreures

--- projet/js/main.js
+++ projet/js/main.js
@@ -19,12 +19,12 @@ function createTitle(myList,size,title) {
   }
 
 
-  function createVideoElement(video) {
+  function createVideoElement(video,i) {
     const templateListe = createList(video.actions);
     const templateObjetsDuDomaine = createTitle(video.objets_du_domaine,"h4","Objets du domaine");
     const templateInstruments = createTitle(video.instruments,"h5","Instruments");
     const templateDiv = `
-      <h1>${video.titre}</h1>
+      <h1> Etape ${i} : ${video.titre}</h1>
       <div class="wrapper">
         <div class="one">
           <img style="width: 100%; height: auto;" src="${video.lien_image}" id="i_${video.id}" onclick="showVideo('${video.id}')" />
@@ -60,8 +60,10 @@ function createTitle(myList,size,title) {
   function ecrireHtml() {
     var node = document.getElementById("ecrire");
     node.innerHTML = ""; // Initialize the innerHTML property to an empty string
+    let i = 1;
     videos.forEach(video => {
-      node.innerHTML += createVideoElement(video);
+      node.innerHTML += createVideoElement(video,i);
+      i++;
     });
   }
   
@@ -73,7 +75,7 @@ function createTitle(myList,size,title) {
 var videos=[
     {
     "id": "step2",
-    "objets_du_domaine": ["cube",  "plan"],
+    "objets_du_domaine": ["plan","Objets 3D"],
     "titre": "Creation de la base",
     "lien_image": "./img/step2.png",
     "lien_video": "./video/step2.mkv",
@@ -91,17 +93,16 @@ var videos=[
   },
   {
     "id": "step3",
-    "objets_du_domaine": ["cube"],
+    "objets_du_domaine": ["Objets 3D","plan"],
     "titre": "Création du toit (partie 1)",
     "lien_image": "./img/step3.png",
     "lien_video": "./video/step3.mkv",
     "blendLink" : "./blender/step3.blend",
-    "instruments": ["sélection", "duplication", "séparation"],
+    "instruments": ["sélection", "échelle"],
     "actions": [
     {"action": "Scube" },
     {"action": "TabE"},
     {"action": "SX"},
-    {"action": "Clickg"},
     {"action": "n3"},
     {"action": "ShiftClickSup"},
     {"action": "ShiftD"},
@@ -111,17 +112,19 @@ var videos=[
     },
     {
       "id": "step5",
-      "objets_du_domaine": ["cube"],
+      "objets_du_domaine": ["plan","Objets 3D"],
       "titre": "Création du toit (partie 2)",
       "lien_image": "./img/step5.png",
       "lien_video": "./video/step5.mkv",
       "blendLink" : "./blender/step5.blend",
-      "instruments": ["sélection", "duplication", "séparation"],
+      "instruments": ["sélection", "extrusion","échelle"],
       "actions": [
       {"action": "TabO" },
       {"action": "Clicktoit"},
       {"action": "TabE"},
       {"action": "A"},
+       {"action": "E"},
+         {"action": "A"},
       {"action": "S"},
       {"action": "Smilieu"},
       {"action": "ShiftD"},
@@ -131,12 +134,12 @@ var videos=[
       },
       {
         "id": "step6",
-        "objets_du_domaine": ["cube"],
+        "objets_du_domaine": ["Objets 3D"],
         "titre": "Création de la poutre ",
         "lien_image": "./img/step6.png",
         "lien_video": "./video/step6.mkv",
         "blendLink" : "./blender/step6.blend",
-        "instruments": ["sélection", "duplication", "séparation"],
+        "instruments": ["sélection", "échelle","extrusion","déplacement"],
         "actions": [
         {"action": "TabO" },
         {"action": "Smilieu"},
@@ -148,43 +151,39 @@ var videos=[
         {"action": "GY"},
         {"action": "Sback"},
         {"action": "S"},
-        {"action": "clickC"},
-        {"action": "GD"},
-        {"action": "Clickg"}
+        {"action": "G"},
         ]
         },
         {
           "id": "step7",
-          "objets_du_domaine": ["cube"],
+          "objets_du_domaine": ["Objets 3D"],
           "titre": "Styliser la forme de la maison",
           "lien_image": "./img/step7.png",
           "lien_video": "./video/step7.mkv",
           "blendLink" : "./blender/step7.blend",
-          "instruments": ["sélection", "duplication", "séparation"],
+          "instruments": ["sélection", "déplacement", "échelle"],
           "actions": [
           {"action": "TabO" },
           {"action": "nom"},
           {"action": "StP"},
           {"action": "TabE"},
           {"action": "A"},
-          {"action": "GD"},
-          {"action": "clickC"},
+          {"action": "G"},
           {"action": "TabO"},
           {"action": "StM"},
           {"action": "TabE"},
           {"action": "Clickinf"},
-          {"action": "S"},
-          {"action": "Clickg"}
+          {"action": "S"}
           ]
           },
           {
             "id": "step8",
-            "objets_du_domaine": ["cube"],
+            "objets_du_domaine": ["Objets 3D","plan"],
             "titre": "Creation de la porte",
             "lien_image": "./img/step8.png",
             "lien_video": "./video/step8.mkv",
             "blendLink" : "./blender/step8.blend",
-            "instruments": ["sélection", "duplication", "séparation"],
+            "instruments": ["sélection", "échelle", "extrusion","déplacement"],
             "actions": [
             {"action": "TabO" },
             {"action": "StM"},
@@ -192,7 +191,7 @@ var videos=[
             {"action": "n3"},
             {"action": "Sfront"},
             {"action": "ShiftD"},
-            {"action": "clickd"},
+            {"action": "Clickd"},
             {"action": "S"},
             {"action": "n2"},
             {"action": "Sbinf"},
@@ -214,12 +213,12 @@ var videos=[
             },
             {
               "id": "step9",
-              "objets_du_domaine": ["cube"],
+              "objets_du_domaine": ["Objets 3D","plan"],
               "titre": "Creation des fenetres",
               "lien_image": "./img/step9.png",
               "lien_video": "./video/step9.mkv",
               "blendLink" : "./blender/step9.blend",
-              "instruments": ["sélection", "duplication", "séparation"],
+              "instruments": ["sélection", "extrusion", "séparation","rotation"],
               "actions": [
               {"action": "TabO" },
               {"action": "StM"},
@@ -227,7 +226,7 @@ var videos=[
               {"action": "n3"},
               {"action": "Clickcote"},
               {"action": "ShiftD"},
-              {"action": "clickd"},
+              {"action": "Clickd"},
               {"action": "S"},
               {"action": "E"},
               {"action": "Sporte"},
@@ -245,12 +244,12 @@ var videos=[
               },
               {
                 "id": "step10",
-                "objets_du_domaine": ["cube"],
+                "objets_du_domaine": ["cercle","Objets 3D"],
                 "titre": "Creation de la fenetre ronde",
                 "lien_image": "./img/step10.png",
                 "lien_video": "./video/step10.mkv",
                 "blendLink" : "./blender/step10.blend",
-                "instruments": ["sélection", "duplication", "séparation"],
+                "instruments": ["sélection", "extrusion", "échelle"],
                 "actions": [
                 {"action": "TabO" },
                 {"action": "StM"},
@@ -267,19 +266,19 @@ var videos=[
                 {"action": "A"},
                 {"action": "E"},
                 {"action": "ShiftA"},
-                {"action": "cube"},
+                {"action": "cude"},
                 {"action": "S"},
                 {"action": "Ec"}
                 ]
                 },
                 {
                   "id": "step11",
-                  "objets_du_domaine": ["cube"],
+                  "objets_du_domaine": ["Objets 3D"],
                   "titre": "Redimentionner la maison",
                   "lien_image": "./img/step11.png",
                   "lien_video": "./video/step11.mkv",
                   "blendLink" : "./blender/step11.blend",
-                  "instruments": ["sélection", "duplication", "séparation"],
+                  "instruments": ["sélection", "échelle"],
                   "actions": [
                   {"action": "TabO" },
                   {"action": "A"},
@@ -291,12 +290,12 @@ var videos=[
                   },
                   {
                     "id": "step12",
-                    "objets_du_domaine": ["cube"],
+                    "objets_du_domaine": ["plan","Objets 3D"],
                     "titre": "Création de la cheminé (partie 1)",
                     "lien_image": "./img/step12.png",
                     "lien_video": "./video/step12.mkv",
                     "blendLink" : "./blender/step12.blend",
-                    "instruments": ["sélection", "duplication", "séparation"],
+                    "instruments": ["sélection", "déplacement", "extrusion"],
                     "actions": [
                     {"action": "TabO" },
                     {"action": "Clicktoit"},
@@ -312,17 +311,20 @@ var videos=[
                     {"action": "virgG"},
                     {"action": "E"},
                     {"action": "Clickd"},
-                    {"action": "GD"}
+                    {"action": "GD"},
+                    {"action": "SZ0"}
+
+              
                     ]
                     },
                     {
                       "id": "step12bis",
-                      "objets_du_domaine": ["cube"],
+                      "objets_du_domaine": ["plan","Objets 3D"],
                       "titre": "Création de la cheminé (partie 2)",
                       "lien_image": "./img/step12bis.png",
                       "lien_video": "./video/step12bis.mkv",
                       "blendLink" : "./blender/step12bis.blend",
-                      "instruments": ["sélection", "duplication", "séparation"],
+                      "instruments": ["sélection", "échelle", "extrusion"],
                       "actions": [
                       {"action": "E" },
                       {"action": "Clickd"},
@@ -335,12 +337,12 @@ var videos=[
                       },
                       {
                         "id": "step13",
-                        "objets_du_domaine": ["cube"],
+                        "objets_du_domaine": ["plan","Objets 3D"],
                         "titre": "Styliser le toit (partie 1)",
                         "lien_image": "./img/step13.png",
                         "lien_video": "./video/step13.mkv",
                         "blendLink" : "./blender/step13.blend",
-                        "instruments": ["sélection", "duplication", "séparation"],
+                        "instruments": ["sélection", "extrusion", "échelle"],
                         "actions": [
                           {"action": "TabO" },
                           {"action": "Clicktoit"},
@@ -360,12 +362,12 @@ var videos=[
                         },
                         {
                           "id": "step13bis",
-                          "objets_du_domaine": ["cube"],
+                          "objets_du_domaine": ["Objets 3D"],
                           "titre": "Styliser le toit (partie 2)",
                           "lien_image": "./img/step13bis.png",
                           "lien_video": "./video/step13bis.mkv",
                           "blendLink" : "./blender/step13bis.blend",
-                          "instruments": ["sélection", "duplication", "séparation"],
+                          "instruments": ["sélection", "rotation"],
                           "actions": [
                           {"action": "TabO" },
                           {"action": "Clickface"},
@@ -378,12 +380,12 @@ var videos=[
                           },
                           {
                             "id": "step14",
-                            "objets_du_domaine": ["cube"],
+                            "objets_du_domaine": ["Objets 3D"],
                             "titre": "Ajout de 'Modifier'",
                             "lien_image": "./img/step14.png",
                             "lien_video": "./video/step14.mkv",
                             "blendLink" : "./blender/step14.blend",
-                            "instruments": ["sélection", "duplication", "séparation"],
+                            "instruments": ["sélection", "modificateurs"],
                             "actions": [
                             {"action": "TabO" },
                             {"action": "StM"},
@@ -393,7 +395,7 @@ var videos=[
                             },
                             {
                               "id": "step15",
-                              "objets_du_domaine": ["cube"],
+                              "objets_du_domaine": ["Objets 3D"],
                               "titre": "Dupliquer la maison",
                               "lien_image": "./img/step15.png",
                               "lien_video": "./video/step15.mkv",
@@ -411,12 +413,12 @@ var videos=[
                               },
                               {
                                 "id": "step16",
-                                "objets_du_domaine": ["cube"],
+                                "objets_du_domaine": ["Objets 3D","Caméras","Lumières","Matériaux"],
                                 "titre": "Ajout camera et couleurs",
                                 "lien_image": "./img/step16.png",
                                 "lien_video": "./video/step16.mkv",
                                 "blendLink" : "./blender/step16.blend",
-                                "instruments": ["sélection", "duplication", "séparation"],
+                                "instruments": ["sélection", "propriété des matériaux"],
                                 "actions": [
                                 {"action": "coll" },
                                 {"action": "ShiftA"},
@@ -437,12 +439,12 @@ var videos=[
                                 },
                                 {
                                   "id": "step17",
-                                  "objets_du_domaine": ["cube"],
+                                  "objets_du_domaine": ["Objets 3D","Matériaux"],
                                   "titre": "Ajout d'effets lumineux",
                                   "lien_image": "./img/step17.png",
                                   "lien_video": "./video/step17.mkv",
                                   "blendLink" : "./blender/step17.blend",
-                                  "instruments": ["sélection", "duplication", "séparation"],
+                                  "instruments": ["sélection", "déplacement", "propriété des matériaux"],
                                   "actions": [
                                   {"action": "TabE" },
                                   {"action": "SporteInt"},
@@ -468,28 +470,28 @@ var correspondance = {
   coll : "Nouvelle Collection",
   cam :  "Choisir une camera",
   ctrlAlt0 : "Ctrl + Alt + 0 : Modifier l'angle",
-  lum :  "Choisir un lumière",
+  lum :  "Choisir une lumière",
   sepV: "Séparer la vue",
   Ombr : "Mode Ombrage de Vue",
   OMR2 : "Ombrage : Eclairage de scene, environnement de la scene",
-  Word : "Dans Monde modifier la couleur du Monde",
+  Word : "Dans 'Propriété du Monde' modifier la couleur",
   propM: "Dans 'Propriété matériaux' :  Nouveau",
   col : "Modifier la couleur",
   emiss : " Dans 'Surface' choisir 'Emission'",
   flou : "Dans 'Propriété du rendu' choisir 'flou lumineux'",
   SupPorte : "Supprimer la porte",
-  modif : "Dans 'Ajouter Modifier' choisir 'Biseau' et moidifier les parametre au choix ",
-  repet: "Repeter pour chacune des éléments",
+  modif : "Dans 'Ajouter Modifier' choisir 'Biseau' et modifier les paramètres au choix ",
+  repet: "Repeter pour chacun des éléments",
   decoup : "Subdiviser le toit plusieurs fois",
   I : "I : Creation des bords",
   SZ0 : "S + Z + 0 : Redimentionner pour obtenir un angle droit",
   virgN :", : Choisir le mode Normal",
   virgG :", : Choisir le mode Global",
   Ec : "E : Extruder les quatres faces",
-  addC : " Alignement = Vue, Verticles =8, Ajuster le radian 0.3",
+  addC : " Alignement=Vue, Sommet=8, rayon=0.3",
   ShiftCd : "Shift + Clic droit : Deplacer le curseur 3d",
   cercle :  "Choisir un cercle",
-  cube :  "Choisir un cude",
+  cube :  "Choisir un cube ",
   ShiftA : "Shift + A : ouvrir le menu d'ajout d'objet",
   plan :  "Choisir un plan",
   TabE : "Tab : Entrer en mode édition",
@@ -518,6 +520,7 @@ var correspondance = {
   GY : "G + Y : Déplacer sur l'axe Y",
   GX : "G + X : Déplacer sur l'axe X",
   GD : "G + D : Déplacer sur l'axe D",
+   G : "G : Déplacer",
   nom : "Renomer les plans",
   clickC : "Click molette",
   StP : "Sélectionner le toit et la poutre",
